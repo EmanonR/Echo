bf9cc4a7ebc89a714cb3b4cca7c4f4fced74f65e
Emanon <155685858+EmanonR@users.noreply.github.com>
2026-06-09 21:34:56 +0200

    Update EchoHandler.cs

--- Scripts/EchoHandler.cs
+++ Scripts/EchoHandler.cs
@@ -2,15 +2,56 @@ using UnityEngine;
 
 public class EchoHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public AnimationClip anim;
+    public PlayerHitBox hitBox;
+    public Vector2 velocity;
+    public int damage;
+    public float waitTime = 1f;
+    public float timer;
+
+    Animator animator;
+    PlayerCollisionHandler colHandler;
+    Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        colHandler = GetComponentInChildren<PlayerCollisionHandler>();
+        rb = GetComponentInChildren<Rigidbody2D>();
+        timer = waitTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            PlayAttack();
+
+            ValidateExit();
+        }
+    }
+
+    public void PlayAttack()
+    {
+        hitBox.EnableHitBox();
+        rb.WakeUp();
+        rb.linearVelocity = velocity;
+        AnimatorOverrideController ov = new(animator.runtimeAnimatorController);
+        ov["Attack"] = anim;
+
+        animator.runtimeAnimatorController = ov;
+        animator.Play("Attack");
+    }
+
+    void ValidateExit()
+    {
+
+    }
+
+    void Dissable()
+    {
+        Destroy(gameObject);
     }
 }
