dd411d305a61c9940072b74a445d9dae8ae44433
Emanon <155685858+EmanonR@users.noreply.github.com>
2026-06-09 20:05:10 +0200

    Update Attackable.cs

--- Scripts/Attackable.cs
+++ Scripts/Attackable.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using System.Collections;
+using TMPro;
 
 public class Attackable : MonoBehaviour
 {
     public int HP = 40;
     public bool imortal;
+    public GameObject hitNumberPrefab;
+    public float numberSpawnHeight;
 
     public virtual void TakeDamage(int damage)
     {
+        GameObject nbr = Instantiate(hitNumberPrefab, transform.position, Quaternion.identity);
+        nbr.GetComponentInChildren<TMP_Text>().text = damage.ToString();
+        nbr.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(Random.Range(-5, 5), Random.Range(5, 10));
+
+        StartCoroutine(DespawnNumber(nbr));
+
         if (imortal) return;
 
         HP -= damage;
@@ -15,8 +25,20 @@ public class Attackable : MonoBehaviour
             Death();
     }
 
+    public IEnumerator DespawnNumber(GameObject obj)
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(obj);
+    }
+
     public virtual void Death()
     {
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position + new Vector3(0, numberSpawnHeight), .4f);
     }
 }
