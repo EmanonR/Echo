d70df93568a9fa96719a7989700e089d66f0f098
Emanon <155685858+EmanonR@users.noreply.github.com>
2026-06-09 18:24:44 +0200

    Create PlayerHitBox.cs

---
+++ PlayerHitBox.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerHitBox : MonoBehaviour
+{
+    public int damage;
+
+    public Collider2D hitBox;
+
+    private void Awake()
+    {
+        hitBox = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Attackable other = collision.GetComponent<Attackable>();
+
+        if (other == null) return;
+
+        other.TakeDamage(damage);
+    }
+
+    public void EnableHitBox()
+    {
+        hitBox.enabled = true;
+    }
+
+    public void DissableHitBox()
+    {
+        hitBox.enabled = false;
+    }
+}
