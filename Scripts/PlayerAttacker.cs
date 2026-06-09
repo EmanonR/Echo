5e9d94ea6500ceda9d0f9c8af27e63b808900e18
Emanon <155685858+EmanonR@users.noreply.github.com>
2026-06-09 18:55:13 +0200

    Update PlayerAttacker.cs

--- Scripts/PlayerAttacker.cs
+++ Scripts/PlayerAttacker.cs
@@ -5,10 +5,11 @@ using System.Collections.Generic;
 
 public class PlayerAttacker : MonoBehaviour
 {
-    public List<AttackSO> Combo;
+    public List<Combo> combos;
     float lastAttackedTime;
     float lastComboEnd;
     int comboCounter;
+    int currentCombo;
 
     public KeyCode attackKey = KeyCode.J;
 
@@ -23,14 +24,73 @@ public class PlayerAttacker : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(attackKey))
-            Attack();
+        {
+            currentCombo = ValidateAndFindAttack();
+
+            if (currentCombo != -1)
+                Attack();
+        }
 
         ExitAttack();
     }
 
+    int ValidateAndFindAttack()
+    {
+        //cycle all combos
+        for (int i = 0; i < combos.Count; i++)
+        {
+            //Check for matching requirements
+            bool validation = CheckRequirements(combos[i].activationReq);
+
+            if (validation)
+                return i;
+        }
+
+        return -1;
+    }
+
+    bool CheckRequirement(Requirement req)
+    {
+        switch (req)
+        {
+            case Requirement.Grounded:
+                if (PlayerController.below)
+                    return true;
+                break;
+
+            case Requirement.AirBorn:
+                if (!PlayerController.below)
+                    return true;
+                break;
+
+            case Requirement.Moving:
+                if (PlayerController.horizontal != 0)
+                    return true;
+                break;
+        }
+
+        return false;
+    }
+
+    bool CheckRequirements(List<Requirement> req)
+    {
+        bool val = true;
+
+        for (int i = 0; i < req.Count; i++)
+        {
+            if (CheckRequirement(req[i]) == false)
+            {
+                val = false;
+                break;
+            }
+        }
+
+        return val;
+    }
+
     void Attack()
     {
-        if (Time.time - lastComboEnd > .1f && comboCounter < Combo.Count)
+        if (Time.time - lastComboEnd > .1f && comboCounter < combos[currentCombo].attacks.Count)
         {
             CancelInvoke(nameof(EndCombo));
 
@@ -38,13 +98,13 @@ public class PlayerAttacker : MonoBehaviour
             {
                 AnimatorController.attacking = true;
                 print("ATTACK!");
-                animator.runtimeAnimatorController = Combo[comboCounter].animatorOV;
+                animator.runtimeAnimatorController = combos[currentCombo].attacks[comboCounter].animatorOV;
                 AnimatorController.PlayAnimation("Attack");
-                hitBox.damage = Combo[comboCounter].damage;
+                hitBox.damage = combos[currentCombo].attacks[comboCounter].damage;
                 comboCounter++;
                 lastAttackedTime = Time.time;
 
-                if (comboCounter > Combo.Count)
+                if (comboCounter > combos[currentCombo].attacks.Count)
                     comboCounter = 0;
             }
         }
@@ -64,4 +124,18 @@ public class PlayerAttacker : MonoBehaviour
         lastComboEnd = Time.time;
         AnimatorController.attacking = false;
     }
+}
+
+[System.Serializable]
+public class Combo
+{
+    public List<Requirement> activationReq;
+    public List<AttackSO> attacks;
+}
+
+public enum Requirement
+{
+    Grounded,
+    AirBorn,
+    Moving
 }
