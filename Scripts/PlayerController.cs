879d8caeaeb3d7332162d961d4a73f658a09ac3d
Emanon <155685858+EmanonR@users.noreply.github.com>
2026-06-09 21:35:25 +0200

    Update PlayerController.cs

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -9,23 +9,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float jumpPower;
     [SerializeField] int maxJumps;
 
-    [Header("Ground")]
-    [SerializeField] float groundedRayHeight;
-    [SerializeField] float groundedRayLength;
-    [SerializeField] LayerMask groundedRayMask;
-
-    [Header("Walls")]
-    [SerializeField] float sideRayBot;
-    [SerializeField] float sideRayTop, sideRayWidth, sideRayLength;
-    [SerializeField] LayerMask sideRayMask;
-
     public static float horizontal;
-
-    public static RaycastHit2D below, left, right;
 
     public static event Action onJump;
     public static bool moveOverride;
     public static PlayerController Instance;
+    PlayerCollisionHandler colHandler;
 
     int jumpCounter;
     Rigidbody2D rb;
@@ -33,7 +22,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-
+        colHandler = GetComponent<PlayerCollisionHandler>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -47,28 +36,20 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        below = CheckColl(groundedRayLength, new Vector2(0, groundedRayHeight), Vector2.down, groundedRayMask);
-        left = CheckColl(sideRayLength, new Vector2(sideRayWidth, sideRayBot), Vector2.left, sideRayMask);
-        right = CheckColl(sideRayLength, new Vector2(-sideRayWidth, sideRayBot), Vector2.right, sideRayMask);
 
-        if (below) jumpCounter = maxJumps;
+        if (colHandler.below) jumpCounter = maxJumps;
         Movement();
-    }
-
-    RaycastHit2D CheckColl(float length, Vector2 offset, Vector2 dir, LayerMask mask)
-    {
-        return Physics2D.Raycast(transform.position + (Vector3)offset, dir, length, mask);
     }
 
     void Movement()
     {
         if (moveOverride) return;
 
-        if (below.collider == null) return;
+        if (colHandler.below.collider == null) return;
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (left.collider != null && horizontal < 0) return;
-        if (right.collider != null && horizontal > 0) return;
+        if (colHandler.left.collider != null && horizontal < 0) return;
+        if (colHandler.right.collider != null && horizontal > 0) return;
 
         rb.linearVelocity = new Vector2(horizontal * moveSpeed, rb.linearVelocityY);
     }
@@ -89,39 +70,9 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = new Vector3(xVel, newVel.y);
     }
 
-
-
     IEnumerator RemoveJump()
     {
         yield return new WaitForSeconds(.1f);
         jumpCounter--;
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Vector2 groundedPos = new Vector2(transform.position.x, transform.position.y + groundedRayHeight);
-
-        Vector2 LB = new Vector2(transform.position.x + sideRayWidth, transform.position.y + sideRayBot);
-        Vector2 LT = new Vector2(transform.position.x + sideRayWidth, transform.position.y + sideRayTop);
-        Vector2 RB = new Vector2(transform.position.x - sideRayWidth, transform.position.y + sideRayBot);
-        Vector2 RT = new Vector2(transform.position.x - sideRayWidth, transform.position.y + sideRayTop);
-
-
-        Gizmos.color = Color.black;
-        Gizmos.DrawWireSphere(groundedPos, .1f);
-
-        Gizmos.DrawWireSphere(LB, .1f);
-        Gizmos.DrawWireSphere(LT, .1f);
-        Gizmos.DrawWireSphere(RB, .1f);
-        Gizmos.DrawWireSphere(RT, .1f);
-
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(groundedPos, Vector3.down * groundedRayLength);
-
-        Gizmos.DrawRay(LB, Vector3.left * sideRayLength);
-        Gizmos.DrawRay(LT, Vector3.left * sideRayLength);
-        Gizmos.DrawRay(RB, Vector3.right * sideRayLength);
-        Gizmos.DrawRay(RT, Vector3.right * sideRayLength);
     }
 }
